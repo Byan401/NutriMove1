4d9b627850012e32693452ba95348ce9412e15fb
Byan401 <byanhussein4@gmail.com>
2026-07-23 00:28:03 +0300

    update URL

--- NutriMove/utils/constants.ts
+++ NutriMove/utils/constants.ts
@@ -1,4 +1,5 @@
 // Backend API URL - your PC's local IP (from ipconfig IPv4)
-const LOCAL_IP = '192.168.1.105';
+// const LOCAL_IP = '192.168.1.100';
 // Physical device via Expo Go always uses the local network IP
-export const API_URL = `http://${LOCAL_IP}:8001`;
+// export const API_URL = `http://${LOCAL_IP}:8001`;
+export const API_URL = "https://nutrimove1-1.onrender.com";
